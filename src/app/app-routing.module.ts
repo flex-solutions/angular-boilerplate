ad19efd559461774532b96bb3d1e1c383278b294
Tai Le <ductai.infotech@gmail.com>
2018-05-15 02:56:03 +0700

    Add user component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes, Router } from '@angular/router';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { MenuComponent } from './menu/menu.component';
+import { UserComponent } from './user/user.component';
 
 const routes: Routes = [
   {
@@ -16,6 +17,13 @@ const routes: Routes = [
     component: MenuComponent,
     data: {
       breadcrumb: 'Menu'
+    }
+  },
+  {
+    path: 'user',
+    component: UserComponent,
+    data: {
+      breadcrumb: 'View User Detail'
     }
   }
 ];
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,9 +7,10 @@ import { AppComponent } from './app.component';
 import { AppRoutingModule } from './/app-routing.module';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { MenuComponent } from './menu/menu.component';
+import { UserComponent } from './user/user.component';
 
 @NgModule({
-  declarations: [AppComponent, DashboardComponent, MenuComponent],
+  declarations: [AppComponent, DashboardComponent, MenuComponent, UserComponent],
   imports: [BrowserModule, SharedModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent]
