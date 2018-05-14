219a22cb0227c9a627f57817aa7ff562c01c87a2
Phuong Vo <cw.hrms.dev@gmail.com>
2018-05-15 02:04:46 +0700

    Implement: Define form builder

--- src/app/modules/users/create-user/create-user.component.ts
+++ src/app/modules/users/create-user/create-user.component.ts
@@ -1,15 +1,74 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  Component,
+  OnInit,
+  AfterViewInit,
+  ViewChildren,
+  ElementRef
+} from '@angular/core';
+import {
+  FormGroup,
+  FormBuilder,
+  Validators,
+  FormControlName
+} from '@angular/forms';
+import { GenericValidator } from '../../../shared/validation/generic-validator';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/operator/debounceTime';
+import 'rxjs/add/observable/fromEvent';
+import 'rxjs/add/observable/merge';
 
 @Component({
   selector: 'app-create-user',
   templateUrl: './create-user.component.html',
   styleUrls: ['./create-user.component.css']
 })
-export class CreateUserComponent implements OnInit {
+export class CreateUserComponent implements OnInit, AfterViewInit {
+  @ViewChildren(FormControlName, { read: ElementRef })
+  formControls: ElementRef[];
 
-  constructor() { }
+  userForm: FormGroup;
+  errorMessage: { [key: string]: string } = {};
+  genericValidator: GenericValidator;
+  private validationMessages: { [key: string]: { [key: string]: string } } = {
+    email: {
+      required: 'Please provide email address.',
+      pattern: 'Email address is invalid.'
+    },
+    fullname: {
+      required: 'Please provide fullname.'
+    },
+    username: {
+      required: 'Please provide username.'
+    }
+  };
 
-  ngOnInit() {
+  constructor(private fb: FormBuilder) {
+    // Create an instance of the generic validator
+    this.genericValidator = new GenericValidator(this.validationMessages);
   }
 
+  ngOnInit() {
+    // Build user form
+    this.userForm = this.fb.group({
+      email: [
+        null,
+        [Validators.required, Validators.pattern('[a-z0-9._%+-]+@[a-z0-9.-]+')]
+      ],
+      fullname: [null, [Validators.required]],
+      username: [null, [Validators.required]]
+    });
+  }
+
+  ngAfterViewInit() {
+    const controlBlurs: Observable<any>[] = this.formControls.map(
+      (formControl: ElementRef) =>
+        Observable.fromEvent(formControl.nativeElement, 'blur')
+    );
+
+    Observable.merge(this.userForm.valueChanges, ...controlBlurs)
+      .debounceTime(1000)
+      .subscribe(value => {
+        this.errorMessage = this.genericValidator.validate(this.userForm);
+      });
+  }
 }
--- src/app/modules/users/users.module.ts
+++ src/app/modules/users/users.module.ts
@@ -1,11 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
 import { UsersRoutingModule } from './users-routing.module';
 import { CreateUserComponent } from './create-user/create-user.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
-  imports: [CommonModule, UsersRoutingModule],
+  imports: [CommonModule, UsersRoutingModule, FormsModule, ReactiveFormsModule],
   declarations: [CreateUserComponent]
 })
 export class UsersModule {}
