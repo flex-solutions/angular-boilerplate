45a4a43eaba0f9e68f524e3f33721138def8c15b
Hieu Tran <hieut.tran@hotschedules.com>
2018-05-22 15:38:41 +0700

    Add dialog module

--- src/app/shared/ui-common/ui-common.module.ts
+++ src/app/shared/ui-common/ui-common.module.ts
@@ -1,17 +1,16 @@
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { LoaderComponent } from './loading-bar/loader.component';
-import { ModalComponent } from './modal/modal.component';
 import { LoaderService } from './loading-bar/loader.service';
 import { NgModule } from '@angular/core';
+import { DialogModule } from './modal/dialog.module';
 
 @NgModule({
   imports: [CommonModule, RouterModule],
   exports: [
-    LoaderComponent
+    LoaderComponent, DialogModule
   ],
   declarations: [
-    ModalComponent,
     LoaderComponent,
   ],
   providers: [
