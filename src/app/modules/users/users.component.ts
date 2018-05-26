6665c65aa597660b3a53de54b9a04ecf1bf2c1a9
Tai Le <ductai.infotech@gmail.com>
2018-05-26 10:07:30 +0700

    Update datagrid with app-dg

--- src/app/modules/users/users.component.ts
+++ src/app/modules/users/users.component.ts
@@ -1,121 +1,114 @@
 import { Component, OnInit, PipeTransform, Pipe, } from '@angular/core';
-import * as jquery from 'jquery';
 import { Router, ActivatedRoute } from '@angular/router';
 import { userConfiguration } from '../user.configuration';
 import { ModalSize } from '../../shared/ui-common/modal/components/dialog.component';
 import { ExDialog } from '../../shared/ui-common/modal/services/ex-dialog.service';
 import { GroupUserModalComponent } from './component/group-user-modal';
+import { Observable, of } from 'rxjs';
+import { IFilterChangedEvent } from '../../shared/ui-common/datagrid/components/datagrid.component';
 
-// declare interface DataTable {
-//      headerRow: string[];
-//      footerRow: string[];
-//      dataRows: string[][];
-// }
-// declare var $: any;
 @Pipe({
-    name: 'demoFilter'
-  })
-  export class FilterPipe implements PipeTransform {
-    transform(items: DataTable[], searchText: string): any[] {
-      if (!items) {
-        return [];
-      }
-      if (!searchText) {
-        return items;
-      }
-      searchText = searchText.toLowerCase();
-      return items.filter(it => {
-        return it.customer.toLowerCase().includes(searchText)
-        || it.shipTo.toLowerCase().includes(searchText);
-      });
+  name: 'demoFilter'
+})
+export class FilterPipe1 implements PipeTransform {
+  transform(items: DataTable[], searchText: string): any[] {
+    if (!items) {
+      return [];
     }
+    if (!searchText) {
+      return items;
+    }
+    searchText = searchText.toLowerCase();
+    return items.filter(it => {
+      return it.customer.toLowerCase().includes(searchText)
+        || it.shipTo.toLowerCase().includes(searchText);
+    });
   }
+}
 
 export class DataTable {
-    orderNumber: number;
-    purchasedOn: Date;
-    customer: string;
-    shipTo: string;
-    basePrice: number;
-    purchasedPrice: number;
-    status: boolean;
-  }
+  orderNumber: number;
+  purchasedOn: Date;
+  customer: string;
+  shipTo: string;
+  basePrice: number;
+  purchasedPrice: number;
+  status: boolean;
+}
 
 @Component({
-     moduleId: module.id,
-     selector: 'app-users',
-     templateUrl: './users.component.html',
-     styleUrls: ['./users.component.css']
+  moduleId: module.id,
+  selector: 'app-users',
+  templateUrl: './users.component.html',
 })
 export class UsersComponent implements OnInit {
 
-     public items: DataTable[] = [];
-     public imagePath = 'https://placehold.it/100x100';
-     constructor(
-          private router: Router,
-          private route: ActivatedRoute,
-          private exDialog: ExDialog) { }
+  public items: DataTable[] = [];
+  public imagePath = 'https://placehold.it/100x100';
+  constructor(
+    private router: Router,
+    private route: ActivatedRoute,
+    private exDialog: ExDialog) { }
 
-     ngOnInit(): void {
-        const item1 = new DataTable();
-        item1.orderNumber = 1;
-        item1.basePrice = 5000000;
-        item1.customer = 'Think Digital';
-        item1.purchasedOn = new Date();
-        item1.purchasedPrice = 500000000;
-        item1.shipTo = 'Quan 1';
-        item1.status = true;
+  ngOnInit(): void {
+    const item1 = new DataTable();
+    item1.orderNumber = 1;
+    item1.basePrice = 5000000;
+    item1.customer = 'Think Digital';
+    item1.purchasedOn = new Date();
+    item1.purchasedPrice = 500000000;
+    item1.shipTo = 'Quan 1';
+    item1.status = true;
 
-        const item2 = new DataTable();
-        item2.orderNumber = 2;
-        item2.basePrice = 6000000;
-        item2.customer = 'SNOB';
-        item2.purchasedOn = new Date();
-        item2.purchasedPrice = 600000000;
-        item2.shipTo = 'Quan go vap';
-        item2.status = false;
+    const item2 = new DataTable();
+    item2.orderNumber = 2;
+    item2.basePrice = 6000000;
+    item2.customer = 'SNOB';
+    item2.purchasedOn = new Date();
+    item2.purchasedPrice = 600000000;
+    item2.shipTo = 'Quan go vap';
+    item2.status = false;
 
-        this.items = [item1, item2];
-        //   this.dataTable = {
-        //        headerRow: ['Full Name', 'Username', 'Login Details', 'Group Name', 'Status', 'Actions'],
-        //        footerRow: [],
-        //        dataRows: [
-        //             ['Airi Satou', 'Andrew Mike', 'Develop', '2013', 'success'],
-        //             ['Angelica Ramos', 'John Doe', 'Design', '2012', 'danger'],
-        //        ]
-        //   };
+    this.items = [item1, item2];
 
-     }
+  }
 
-     getUserInfomation() { }
+  public count = (searchKey: string): Observable<number> => {
+    console.log('only should get count when init datagrid of filter changed');
+    // call count method in data service class
+    return of(300);
+  }
 
-     showConfirm() {
-          this.exDialog.openConfirm('User "hieutran" will be permanently deleted. Are you sure want to delete?',
-               'Confirm', ModalSize.Normal)
-               .subscribe(result => {
-                    if (result) {
-                         alert('Submited');
-                    }
-               });
-     }
+  onPageChanged(eventArg: IFilterChangedEvent) {
+    console.log(eventArg);
+  }
 
-     showChangeUserGroup() {
-          this.exDialog.openPrime(GroupUserModalComponent, 'Change user group', ModalSize.Normal).subscribe(result => {
-               if (result) {
-                    alert('you clicked Submit button');
-               }
-          });
-     }
+  getUserInfomation() { }
 
-     navigateToCreatePage() {
-          this.router.navigate([userConfiguration.createPageUrl], { relativeTo: this.route });
-     }
+  showConfirm() {
+    this.exDialog.openConfirm('User "hieutran" will be permanently deleted. Are you sure want to delete?',
+      'Confirm', ModalSize.Normal)
+      .subscribe(result => {
+        if (result) {
+          alert('Submited');
+        }
+      });
+  }
 
-     navigateToEditPage() {
-          this.router.navigate([userConfiguration.editUserPageUrl], { relativeTo: this.route });
-     }
+  navigateToChangeUserGroup() {
+    // this.router.navigate([userConfiguration.editUserPageUrl]);
+    alert('Redirect to user group page.');
+  }
 
-     navigateToUserDetailPage() {
-          this.router.navigate([userConfiguration.userDetailPage], { relativeTo: this.route });
-     }
+  navigateToCreatePage() {
+    this.router.navigate([userConfiguration.createPageUrl], { relativeTo: this.route });
+  }
+
+  navigateToEditPage() {
+    this.router.navigate([userConfiguration.editUserPageUrl]);
+  }
+
+  navigateToUserDetailPage() {
+    this.router.navigate([userConfiguration.userDetailPage], { relativeTo: this.route });
+  }
 }
--- src/app/modules/users/users.module.ts
+++ src/app/modules/users/users.module.ts
@@ -6,16 +6,16 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { UserDetailComponent } from './user-detail/user-detail.component';
 import { EditUserComponent } from './edit-user/edit-user.component';
 import { UserService } from './services/user.service';
-import { UsersComponent } from './users.component';
+import { UsersComponent, FilterPipe1 } from './users.component';
 import { UsersService } from './users-service';
 import { DatagridModule } from '../../shared/ui-common/datagrid/datagrid.module';
 import { UICommonModule } from '../../shared/ui-common/ui-common.module';
 import { FilterPipe } from '../datagrid-demo/demo/datagrid-demo.component';
+import { GroupUserModalComponent } from './component/group-user-modal';
 
 @NgModule({
   imports: [CommonModule, UsersRoutingModule, FormsModule, ReactiveFormsModule, UICommonModule],
-  declarations: [CreateUserComponent, UserDetailComponent, EditUserComponent, UsersComponent, FilterPipe, GroupUserModalComponent],
-  providers: [UserService, UsersService],
-  entryComponents: [GroupUserModalComponent]
+  declarations: [CreateUserComponent, UserDetailComponent, EditUserComponent, UsersComponent, GroupUserModalComponent, FilterPipe1],
+  providers: [UserService, UsersService]
 })
 export class UsersModule { }
