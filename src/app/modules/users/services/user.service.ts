0116c6b5a8e83ad5c2810db82bf98e4af3fd0f43
Phuong Vo <cw.hrms.dev@gmail.com>
2018-05-27 20:58:30 +0700

    Add delete user

--- src/app/modules/users/services/user.service.ts
+++ src/app/modules/users/services/user.service.ts
@@ -2,11 +2,12 @@ import { User } from './../../../shared/models/user.model';
 import { Injectable } from '@angular/core';
 import { AbstractRestService } from '../../../shared/abstract/abstract-rest-service';
 import { of, Observable } from 'rxjs';
+import { ExDialog } from '../../../shared/ui-common/modal/services/ex-dialog.service';
 
 @Injectable()
 export class UserService extends AbstractRestService {
   protected controllerName: string;
-  constructor() {
+  constructor(private exDialog: ExDialog) {
     super();
     this.controllerName = 'users';
   }
@@ -19,8 +20,18 @@ export class UserService extends AbstractRestService {
     return this.put(user._id, user);
   }
 
-  remove(userId: string) {
-    return this.delete(userId, userId);
+  async remove(user: User) {
+    const confirmMsg = this.translateService.translateWithParams('users-delete-dialog-confirm_message', user.username);
+    await this.exDialog.openConfirm(confirmMsg).subscribe(result => {
+      if (result) {
+        // Submit button has clicked
+        this.delete(user._id, user._id).subscribe(res => {
+          // Show confirm message when delete success
+          const notificationMsg = this.translateService.translateWithParams('users-delete-dialog-notification_message', user.username);
+          this.notifier.showSuccess(notificationMsg);
+        });
+      }
+    });
   }
 
   findOne(userId: string) {
