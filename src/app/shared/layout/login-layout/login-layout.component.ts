66ff9eeb7312f9ae437f28bf3c9599bca8d6160a
Phuong Vo <cw.hrms.dev@gmail.com>
2018-06-03 00:49:16 +0700

    Refactor

--- src/app/shared/layout/login-layout/login-layout.component.ts
+++ src/app/shared/layout/login-layout/login-layout.component.ts
@@ -1,9 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-login-layout-component',
-  templateUrl: './login-layout-component.component.html',
-  styleUrls: ['./login-layout-component.component.css']
+  selector: 'app-login-layout',
+  templateUrl: './login-layout.component.html',
+  styleUrls: ['./login-layout.component.css']
 })
 export class LoginLayoutComponent implements OnInit {
 
