bb53475c72443924011bf43eb456ba12ed9938ec
Thuoc Le <thuocle@fless.vn>
2018-06-05 04:30:44 +0700

    Update sitekey

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  host: 'http://localhost:3000',
-  INVISIBLE_RECAPTCHA_SITEKEY: '6LdJ1lwUAAAAAKHVF2u9DM7D9o9LyLtrC6ANc1B-',
+  host: 'http://13.250.64.90:3000',
+  INVISIBLE_RECAPTCHA_SITEKEY: '6LdiA10UAAAAALuxIB1bCFAYJmNtr1ezF-muzAQF',
   INVISIBLE_RECAPTCHA_SECRETKEY: '6LdJ1lwUAAAAAHmKS2GBNKzemYTNNuYazLrtnRKt'
 };
