9f968c56bb741b227529448b057736db5e94505a
phuongvohoai@outlook.com <phuongvohoai@outlook.com>
2018-06-07 23:59:30 +0700

    Fix bug #110: Validation behavior isn't consistency on user group

--- src/app/modules/permission-scheme/pipes/permission-scheme.pipe.ts
+++ src/app/modules/permission-scheme/pipes/permission-scheme.pipe.ts
@@ -1,5 +1,7 @@
 import { Pipe, PipeTransform } from '@angular/core';
 import { ControllerSelectedItem, PermissionDetail, IPermissionScheme } from '../../../shared/models/permission-scheme.model';
+import { UserGroup } from '../../../shared/models/user-group.model';
+import { SelectAndAddedModel } from '../../../shared/models/selectable.model';
 
 @Pipe({
     name: 'controllerFilter'
@@ -55,8 +57,27 @@ export class PermissionSchemeFilter implements PipeTransform {
     }
 }
 
+@Pipe({
+    name: 'userGroupFilter'
+})
+export class UserGroupFilter implements PipeTransform {
+    transform(items: SelectAndAddedModel<UserGroup>[], searchText: string): any[] {
+        if (!items) {
+            return [];
+        }
+        if (!searchText) {
+            return items;
+        }
+        searchText = searchText.toLowerCase();
+        return items.filter(it => {
+            return it.model.name.toLowerCase().includes(searchText);
+        });
+    }
+}
+
 export const PermissionSchemePipes = [
     PermissionFilterPipe,
     ControllerFilterPipe,
     PermissionSchemeFilter,
+    UserGroupFilter
 ];
