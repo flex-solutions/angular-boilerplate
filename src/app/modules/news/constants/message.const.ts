71abdbebe030ade939043aeb1f0193aaf03bfdd1
Hieu Tran <hieutran@fless.vn>
2018-09-23 22:43:12 +0700

    Enhance message

--- src/app/modules/news/constants/message.const.ts
+++ src/app/modules/news/constants/message.const.ts
@@ -4,4 +4,5 @@ export const NewMessageConst = {
     NewStatus: 'list-news-status-new',
     DeleteSuccessfullyNotification: 'list-news-notification-delete-successfully',
     ConfirmDeletNew: 'list-news-confirm-delete',
+    UnpublishNews: 'list-news-btn-unpublishs',
 };
